ef5969809445aa43a7cda11bf4e9805617fa9830
ckronber <ckronber@gmail.com>
2022-06-10 11:33:22 -0500

    first commit

---
+++ pokeScript.js
@@ -0,0 +1,91 @@
+let pokemonID;
+
+function defaultPokeinfo(){
+    var pokeInput = "charizard";
+    console.log(pokeInput);
+    searchPokedexInfo(pokeInput);
+    searchPokedexSprites(pokeInput);
+}
+
+function getAllPokeinfo(pokemonHere){
+    let pokeInput = document.getElementById("pokeEntry").value.toLowerCase();
+    console.log(pokeInput);
+    searchPokedexInfo(pokeInput);
+    searchPokedexSprites(pokeInput);
+}
+
+function getAllPokeinfo1(pokemonHere){
+  console.log(pokemonHere);
+  searchPokedexInfo(pokemonHere);
+  searchPokedexSprites(pokemonHere);
+}
+
+
+function getID(my_id){
+  document.getElementById("dexNum").innerHTML= "DEX #" + my_id; 
+}
+
+function move(direction){
+  console.log(direction);
+  if(direction == 1){
+    var currentId = pokemonID;
+    currentId+=1;
+    (currentId %= 650);
+    if(currentId == 0){
+      currentId+=1;
+    }
+    getAllPokeinfo1(currentId);}
+  else{
+    var currentId = pokemonID;
+    if(currentId > 1){currentId-=1;}
+    else{
+      currentId = 649;
+    }
+    getAllPokeinfo1(currentId);
+  }
+}
+
+function searchPokedexSprites(info){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var pokeData1 = JSON.parse(this.response);
+      console.log(pokeData1.sprites.front_default);
+      document.getElementById("pokeImage").innerHTML = "<img src=\""+pokeData1.sprites.other.dream_world.front_default+"\">";
+      pokemonID = pokeData1.id;
+      getID(pokemonID);
+
+    
+      console.log(pokeData1.abilities[0].ability.name);
+      document.getElementById("pokeAbilities").innerHTML = "";
+      var aList = [];
+
+      for(var i=0;i<pokeData1.abilities.length;i++){
+        document.getElementById("pokeAbilities").innerHTML += "<li>"+pokeData1.abilities[i].ability.name+"</>";
+
+      }
+
+      document.getElementById("cancer").innerHTML ="";
+    }
+    else{
+      document.getElementById("cancer").innerHTML =  "<div class=\"alert alert-warning alert-dismissible fade show\" role=\"alert\">"+info+" does not exist as a Pokemon<button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button></div>";
+    }
+    };
+  xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/"+info);
+  xhttp.send();
+}
+
+function searchPokedexInfo(info){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var pokeData1 = JSON.parse(this.response);
+     
+      document.getElementById("pokeName").innerHTML = pokeData1.name.replace(pokeData1.name[0],pokeData1.name[0].toUpperCase());
+      document.getElementById("pokeDescription").innerHTML = pokeData1.flavor_text_entries[1].flavor_text.replace("\f"," ");
+    }
+    };
+    
+  xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon-species/"+info);
+  xhttp.send();
+}
